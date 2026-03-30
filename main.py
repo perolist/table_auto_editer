c9b7a84cc47b222a836699ec66d4089b98726b9e
perolist <prolist@icloudl.com>
2026-03-31 00:24:21 +0900

    カレント内のxlsxファイル参照処理追加

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import pandas as pd
-
+from pathlib import Path
 
 def add_column(df, column_name):
     df[column_name] = None
     print("列を追加しました")
     print(df)
 
+target_path = Path('./')
+target_files = list(target_path.glob('*.xlsx'))
 
 print("編集対象のファイル名指定してください")
-target_file = input('')
 
-with pd.ExcelFile(target_file) as xls:
+
+for i, file in enumerate(target_files):
+    print(f"{file} [{i}]")
+
+
+target_file = int(input(''))
+
+
+with pd.ExcelFile(target_files[target_file]) as xls:
     sheets = xls.sheet_names
 
     print("どのシートを編集しますか？")
