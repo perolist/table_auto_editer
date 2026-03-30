db5ac3adbd4fa58110f3d0cc36739f72b892c628
perolist <prolist@icloudl.com>
2026-03-31 00:39:58 +0900

    シート名と一緒にシート番号も出力するよう変更

--- main.py
+++ main.py
@@ -9,26 +9,26 @@ def add_column(df, column_name):
 target_path = Path('./')
 target_files = list(target_path.glob('*.xlsx'))
 
-print("編集対象のファイル名指定してください")
 
+print("編集対象のファイル名指定してください")
 
 for i, file in enumerate(target_files):
     print(f"{file} [{i}]")
-
 
 target_file = int(input(''))
 
 
 with pd.ExcelFile(target_files[target_file]) as xls:
     sheets = xls.sheet_names
-
-    print("どのシートを編集しますか？")
-    print(sheets)
-    sheet_num = int(input(''))
-
     
+    print("どのシートを編集しますか？")
+    for sheet_num, sheet_name in enumerate(sheets):
+        print(f"{sheet_name} [{sheet_num}]")
 
-    edit_target = xls.parse(sheets[sheet_num])
+    target_sheet = int(input(''))
+
+
+    edit_target = xls.parse(sheets[target_sheet])
 
     add_column(edit_target, "好物")
 
